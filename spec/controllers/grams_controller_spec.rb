584bb727fc386283536772690111d1329cc1446c
adriana <adrianamolello@gmail.com>
2016-03-28 18:06:38 +0000

    Added show for grams

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,6 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
+  describe "grams#show action" do
+    it "should successfully show the page if the gram is found" do
+      gram = FactoryGirl.create(:gram)
+      get :show, id: gram.id
+      expect(response).to have_http_status(:success)
+    end
+
+    it "should return a 404 error if the gram is not found" do
+      get :show, id: 'TACOCAT'
+      expect(response).to have_http_status(:not_found)
+      end
+    end
+
+
+
   describe "grams#index action" do
     it "should successfully show the page" do
       get :index
@@ -17,7 +32,7 @@ RSpec.describe GramsController, type: :controller do
 
     it "should successfully show the new form" do
      user = FactoryGirl.create(:user)
-      sign_in user
+     sign_in user
 
       get :new
       expect(response).to have_http_status(:success)
@@ -33,10 +48,11 @@ RSpec.describe GramsController, type: :controller do
 
   	it "should successfully create a new gram in our database" do
   		  user = FactoryGirl.create(:user)
-      sign_in user
+        sign_in user
 
       post :create, gram: {message: 'Hello!'}
   		expect(response).to redirect_to root_path
+
 
   		gram = Gram.last
   		expect(gram.message).to eq("Hello!")
@@ -45,12 +61,8 @@ RSpec.describe GramsController, type: :controller do
 
       it "should properly deal with validation errors" do
         user = FactoryGirl.create(:user)
-      sign_in user
+        sign_in user
+      end
 
-
-        post :create, gram: {message: ''}
-        expect(response).to have_http_status(:unprocessable_entity)
-      expect(Gram.count).to eq 0
-      end 
   end
 end
